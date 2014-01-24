b94bca1f50926e2796f1c0fbc5a2d204670a9d33
jaime <jtbenavente@gmail.com>
2014-01-24 19:53:01 +0100

    actualiza el script de aprovisionamiento

--- Aprovisionamiento/script.sh
+++ Aprovisionamiento/script.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get upgrade
 
 #Instalamos git
-sudo apt-get install git
+sudo apt-get install git -y
 
 #instalamos python
 sudo apt-get install python -y
@@ -15,6 +15,19 @@ sudo apt-get install python -y
 sudo apt-get install python-pip -y
 
 #Configuración e instalación de django
+sudo pip install django
 
+#instalacion de modulos de python
+sudo pip install tweepy
+
+#Clonamos el repositorio de GitHub
+git clone https://github.com/IV-GII/SocialCookies.git
+
+#nos movemos a la carpeta y lo lanzamos
+
+cd SocialCookies/ENV1
+source bin/activate
+cd webcookies
+sudo python manage.py runserver 80
 
 
