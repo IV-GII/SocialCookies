c0f3c3a59d10764ed5bf7d96a19561f700737c3f
jaime <jtbenavente@gmail.com>
2014-01-24 19:38:24 +0100

    añade documentación vacia y script para aprovisionamiento

---
+++ Aprovisionamiento/script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+#Actualizamos la maquina
+sudo apt-get update
+sudo apt-get upgrade
+
+#Instalamos git
+sudo apt-get install git
+
+#instalamos python
+sudo apt-get install python -y
+
+#instalamos el gestor de modulos de python
+sudo apt-get install python-pip -y
+
+#Configuración e instalación de django
+
+
+
