6f3584066799f718ec2b2dad84e8827d6e3d9177
Oscar Zafra <oskyar@gmail.com>
2014-01-25 14:28:41 +0100

    Se cambia el formulario con los estilos de bootstrap

--- ENV1/webcookies/webcookies/settings.py
+++ ENV1/webcookies/webcookies/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE_CLASSES = (
 )
 
 TEMPLATE_DIRS = (
-    '/home/francisco/Documentos/Facultad/SocialCookies/ENV1/webcookies/socialcookies/templates/socialcookies'
+    '/home/oskyar/proyectosGit/SocialCookies/ENV1/webcookies/socialcookies/templates/socialcookies'
 
 )
 
