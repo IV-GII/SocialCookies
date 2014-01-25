f2d9edd7049f60afbefb355810b3744774472c99
pedroag <pedroag@correo.ugr.es>
2014-01-25 16:52:21 +0100

    Update twitter.py

--- Twitter_Python/twitter.py
+++ Twitter_Python/twitter.py
@@ -10,7 +10,7 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
-for tweet in tweepy.Cursor(api.search,q="melero90",count=100,result_type="photo",include_entities=True).items():
+for tweet in tweepy.Cursor(api.search,q="hashtag",count=100,result_type="photo",include_entities=True).items():
     #print tweet.text
     if 'media' in tweet.entities:
         for image in  tweet.entities['media']:
