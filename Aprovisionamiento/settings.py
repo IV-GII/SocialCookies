48a09dc279e1f4bf7738cdbef8cdc7e399911353
rogegg <rogegg@correo.ugr.es>
2014-01-25 16:57:54 +0100

    modificado settings al aprovisionamiento

--- Aprovisionamiento/settings.py
+++ Aprovisionamiento/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE_CLASSES = (
 )
 
 TEMPLATE_DIRS = (
-    '/home/oskyar/proyectosGit/SocialCookies/ENV1/webcookies/socialcookies/templates/socialcookies',
+    '/home/jaime/SocialCookies/ENV1/webcookies/socialcookies/templates/socialcookies',
 )
 
 ROOT_URLCONF = 'webcookies.urls'
