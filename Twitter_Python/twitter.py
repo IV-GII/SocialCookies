f0ad6f5802acd4e5a4dd95235a52478566136b1e
pedroag <pedroag@correo.ugr.es>
2014-01-25 19:20:46 +0100

    Editado

--- Twitter_Python/twitter.py
+++ Twitter_Python/twitter.py
@@ -1,5 +1,10 @@
 import tweepy
+import web
+from web.contrib.template import render_mako
+from web import form
 from bottle import route,run
+
+web.config.debug=False
 
 consumer_key = 'acisTloOHSws1UA289etVw'
 consumer_secret = 'b9eVS9CeFxIFx3jIwKkeeQPfsO3hlrAdWNOfIItQEgU'
@@ -10,37 +15,64 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+urls=(
+	'/', 'Home',
+	'/Mostrar','Mostrar',
+	'/Salida','Salida',	
+)
 
-@route('/')
-def home():
-	tuits=[]
+app=web.application(urls, globals())
 
-	for tweet in tweepy.Cursor(api.search,q="hashtag",count=100,result_type="photo",include_entities=True).items(100):
-    		if 'media' in tweet.entities:
-        		for image in  tweet.entities['media']:
-				tuits.append("<img src='%s'  onClick='imgFunction(this, \"%s\")' />" % (image['media_url'],image['media_url']))
-	tuits = ''.join(tuits)
+render=render_mako(directories=['templates'],input_encoding='utf-8',output_encoding='utf-8')
 
-	return """
-        <script type='text/javascript'>
-        
-		var elegidos = new Array();
-        
-        function imgFunction(objeto, url){
-        	        	
-        	if(objeto.style.opacity==1){
-		    	objeto.style.opacity=0.4;
-		    	elegidos.push(url);
-		    	console.log(elegidos.valueOf(elegidos.length-1));
+formulario=form.Form(
+	form.Textbox("busqueda", form.notnull, description="busqueda"),
+	form.Button("Enviar"),
+)
+
+
+class Home:
+
+	def GET(self):
+		f=formulario()
+		return render.index(formu=f.render())
+
+class Mostrar:
+
+	def POST(self):
+		entrada=web.input()
+		busq=entrada.busqueda
+		
+		tuits=[]
+		for tweet in tweepy.Cursor(api.search,q=busq,count=100,result_type="photo",include_entities=True).items(100):
+    			if 'media' in tweet.entities:
+        			for image in  tweet.entities['media']:
+					tuits.append("<img src='%s'  onClick='imgFunction(this, \"%s\")' />" % (image['media_url'],image['media_url']))
+		tuits = ''.join(tuits)
+		
+		return """
+        	<script type='text/javascript'>
+        	
+			var elegidos = new Array();
+        	
+        	function imgFunction(objeto, url){
+        		        	
+        		if(objeto.style.opacity==1){
+			    	objeto.style.opacity=0.4;
+			    	elegidos.push(url);
+			    	console.log(elegidos.valueOf(elegidos.length-1));
+        		}
+        		else{
+        			objeto.style.opacity=1;
+        			indice = elegidos.indexOf(url);
+        			if(indice!=-1)
+        				elegidos.splice(indice,1);
+        		}
         	}
-        	else{
-        		objeto.style.opacity=1;
-        		indice = elegidos.indexOf(url);
-        		if(indice!=-1)
-        			elegidos.splice(indice,1);
-        	}
-        }
-		</script>
-        Photos from Twitter <br>""" + tuits
+			</script>
+        	Photos from Twitter <br>""" + tuits
+		
+	
 
-run(host='localhost', port=8000, reloader=True)
+if __name__=="__main__":
+	app.run()
